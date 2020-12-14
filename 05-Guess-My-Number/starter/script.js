6837e146cdd6c54269ce6ba538679637d1df0985
Sridhar Kothapalli <sridr8@gmail.com>
2020-12-14 23:37:05 +0530

    querySelector usage

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+document.querySelector('.score').textContent = 19;
+document.querySelector('.highscore').textContent = 20;
+document.querySelector('.guess').value = 24;
+document.querySelector('.number').textContent = 46;
