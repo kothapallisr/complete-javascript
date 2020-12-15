a1f2acf259930659389f594c942ea3f53ba342f9
Sridhar Kothapalli <sridr8@gmail.com>
2020-12-15 16:01:15 +0530

    added event listener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,15 @@
 'use strict';
+/*
 document.querySelector('.score').textContent = 19;
 document.querySelector('.highscore').textContent = 20;
 document.querySelector('.guess').value = 24;
 document.querySelector('.number').textContent = 46;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
