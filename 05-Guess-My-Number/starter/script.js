ee3b1bcaf981c2aaf78ac06ba1ea14e70e67b060
Sridhar Kothapalli <sridr8@gmail.com>
2020-12-16 14:59:27 +0530

    high score functionality implemented

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -7,6 +7,7 @@ document.querySelector('.number').textContent = 46;
 */
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 //document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -18,6 +19,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('body').style.backgroundColor = '#60b347';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = '📈 Too High!';
     score--;
