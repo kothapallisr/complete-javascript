b161649984e7e370c048d1fd8db22c1359226c7f
Sridhar Kothapalli <sridr8@gmail.com>
2020-12-16 14:10:11 +0530

    updated logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,11 +5,32 @@ document.querySelector('.highscore').textContent = 20;
 document.querySelector('.guess').value = 24;
 document.querySelector('.number').textContent = 46;
 */
-
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+//document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+    score--;
+    if (score > 0) {
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🔥 You lost the Game';
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+    score--;
+    if (score > 0) {
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🔥 You lost the Game';
+    }
   }
 });
