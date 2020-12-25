82c352fac98e39c72728e358b5e24abe5fe14056
Sridhar Kothapalli <sridr8@gmail.com>
2020-12-25 07:42:26 +0530

    implemented the open, close modal and overlay functionalities

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,7 +3,20 @@ let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 let btnCloseModel = document.querySelector('.close-modal');
 let btnShowModel = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 console.log(btnShowModel);
 for (let i = 0; i < btnShowModel.length; i++) {
-  console.log(btnShowModel[i].textContent);
+  btnShowModel[i].addEventListener('click', openModal);
 }
+btnCloseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
