203f4bf01a4de75a799de26460c53cb8595a11fa
Sridhar Kothapalli <sridr8@gmail.com>
2021-01-11 21:20:15 +0530

    format

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
+// Modal window //
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
